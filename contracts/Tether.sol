cebb7fa413b009e9cfd327bec36dceaf07ccee73
Cristian San <cristiansan@gmail.com>
2024-12-07 16:46:38 -0300

    renombré el archivo
    debería llamarse Tether (en vez de thether) tiene una H de más

--- contracts/Tether.sol
+++ contracts/Tether.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Thether is ERC20 {
+contract Tether is ERC20 {
     constructor() ERC20("Thether", "USDT") {
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
